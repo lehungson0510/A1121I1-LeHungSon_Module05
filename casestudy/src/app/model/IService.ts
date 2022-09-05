07d62aacf01e97046b9859a0bc196bd1836d1ae7
lehungson0510 <hungsonle0510@gmail.com>
2022-09-05 13:00:07 +0700

    up case study 5/9/2022

---
+++ casestudy/src/app/model/IService.ts
@@ -0,0 +1,14 @@
+export interface IService {
+  id?: number;
+  name?: string;
+  area?: number;
+  image?:string;
+  cost?: number;
+  maxPeople?: number;
+  rentTypeId?: number;
+  serviceTypeId?: number;
+  standard?: string;
+  description?: string;
+  poolArea?: number;
+  floors?: number
+}
